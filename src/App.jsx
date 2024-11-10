f91bb74bc9a4e98f013d4598056befb79595c849
nooralam <mdnooralam66266@gmail.com>
2024-11-11 01:40:03 +0530

    second commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import Navbar from "./Navbar1";
+
+
+function App() {
+  return (
+    <div className="App">
+        Hello moon
+     <Navbar />
+    </div>
+  ); 
+}
+
+export default App;
